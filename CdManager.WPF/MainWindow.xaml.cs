242f988e7099390d723b2a1417a07d3b6f156ad5
kd180273 <kd180273@WS10A17>
2020-09-29 20:29:52 +0200

    Add project files.

---
+++ CdManager.WPF/MainWindow.xaml.cs
@@ -0,0 +1,83 @@
+﻿using CdManager.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CdManager.WPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private List<Cd> _cds;
+        private Cd _highlightedCd;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Loaded += MainWindow_Loaded;
+            btnNew.Click += BtnNew_Click;
+            btnDelete.Click += BtnDelete_Click;
+            btnEdit.Click += BtnEdit_Click;
+        }
+
+        private void BtnEdit_Click(object sender, RoutedEventArgs e)
+        {
+            NewCDWindow editWindow = new NewCDWindow(listBoxCds.SelectedItem as Cd);
+            editWindow.ShowDialog();
+
+            Repository repository = Repository.GetInstance();
+            _cds = repository.GetAllCds();
+            listBoxCds.ItemsSource = _cds;
+        }
+
+        private void BtnDelete_Click(object sender, RoutedEventArgs e)
+        {
+            _highlightedCd = listBoxCds.SelectedItem as Cd;
+           
+            Repository repository = Repository.GetInstance();
+
+            if(_highlightedCd != null)
+            {
+                repository.DeleteCd(_highlightedCd);
+            }
+            else
+            {
+                MessageBox.Show("Keine CD ausgewählt!");
+            }
+
+            _cds = repository.GetAllCds();
+            listBoxCds.ItemsSource = _cds;
+        }
+
+        private void BtnNew_Click(object sender, RoutedEventArgs e)
+        {
+            NewCDWindow newWindow = new NewCDWindow(null);
+            newWindow.ShowDialog();
+
+            Repository repository = Repository.GetInstance();
+            _cds = repository.GetAllCds();
+            listBoxCds.ItemsSource = _cds;
+        }
+
+        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            Repository repository = Repository.GetInstance();
+            _cds = repository.GetAllCds();
+            listBoxCds.ItemsSource = _cds;
+        }
+    }
+}
---
+++ CdManager.WPF/Windows/NewCDWindow.xaml.cs
@@ -0,0 +1,69 @@
+﻿using CdManager.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace CdManager.WPF
+{
+    /// <summary>
+    /// Interaction logic for NewCDWindow.xaml
+    /// </summary>
+    public partial class NewCDWindow : Window
+    {
+        private Cd _cd;
+        private bool isNewCd;
+        public NewCDWindow(Cd cd)
+        {
+            InitializeComponent();
+            _cd = cd;
+            Loaded += NewCDWindow_Loaded;
+        }
+
+        private void NewCDWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            btnCancle.Click += BtnCancle_Click;
+            btnSave.Click += BtnSave_Click;
+
+            if(_cd == null)
+            {
+                DataContext = new Cd();
+                isNewCd = true;
+            }
+            else
+            {
+                DataContext = new Cd() { AlbumTitle = _cd.AlbumTitle, Artist = _cd.Artist };
+                isNewCd = false;
+            }
+            
+        }
+
+        private void BtnSave_Click(object sender, RoutedEventArgs e)
+        {
+            if(isNewCd)
+            {
+                Repository.GetInstance().AddCd(DataContext as Cd);
+                this.Close();
+            }
+            else
+            {
+                Repository.GetInstance().DeleteCd(_cd);
+                Repository.GetInstance().AddCd(DataContext as Cd);
+                this.Close();
+            }
+
+        }
+
+        private void BtnCancle_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
